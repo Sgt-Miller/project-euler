f948e42118f774a0184bbde44990f8ef0bddd9b7
Sgt-Miller <ephraim.raj@protonmail.com>
2021-11-15 14:24:22 +0530

    changed wrong filename

--- p3.py
+++ p3.py
@@ -1,17 +1,30 @@
+def is_all_factors(a,list):
+    prod=1
+    for i in list:
+        prod=prod*i
+    return prod == a
 
+def next_prime(n):
+    n=n+1
+    while not isprime(n):
+        n=n+1
+    return n
+def isprime(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
 
-
-
-
-a=1
-b=1
-sum=0
-while b <= 4000000:
-    t=b
-    b=a+b
-    a=t
-
-    if b%2 == 0:
-        sum=sum+b
-print(sum)
-
+l=[]
+n=2
+ques=600851475143
+q=ques
+while True:
+    if ques%n==0 :
+        l.append(n)
+        ques=ques/n
+        continue
+    n=next_prime(n)
+    if is_all_factors(q,l):
+            break
+print(l)
